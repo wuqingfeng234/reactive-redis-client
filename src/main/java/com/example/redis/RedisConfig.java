4b42f9dfce1740020c5643ded3784da5aad832ad
GW00294495 <wuqingfeng234@163.com>
2022-05-07 18:01:15 +0800

    更新redis初始化方式

--- src/main/java/com/example/redis/RedisConfig.java
+++ src/main/java/com/example/redis/RedisConfig.java
@@ -7,21 +7,19 @@ import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-class AppConfig {
+class RedisConfig {
 
     @Bean
     public ReactiveRedisTemplate<String, User> reactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
-        RedisSerializationContext.RedisSerializationContextBuilder<String, User> builder =
-                RedisSerializationContext.newSerializationContext();
-        RedisSerializationContext<String, User> context = builder.key(RedisSerializer.string())
+        RedisSerializationContext.RedisSerializationContextBuilder<String, User> builder = RedisSerializationContext.newSerializationContext();
+        RedisSerializationContext<String, User> context = builder.key(StringRedisSerializer.UTF_8)
                 .value(new Jackson2JsonRedisSerializer<>(User.class))
-                .hashKey(RedisSerializer.string())
-                .hashValue(RedisSerializer.string())
+                .hashKey(StringRedisSerializer.UTF_8)
+                .hashValue(StringRedisSerializer.UTF_8)
                 .build();
-
         return new ReactiveRedisTemplate<>(connectionFactory, context);
     }
 }
